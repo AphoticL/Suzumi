9e9d75ad43acce039c7c8f37f0782a939a4f54da
AphoticL <taikoyuup@hotmail.com>
2017-02-13 22:33:30 +0700

    Updated file to latest version as of 13/2

--- bot.js
+++ bot.js
@@ -1,16 +1,26 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 const prefix = "/";
-// const fs = require("fs");
+const fs = require("fs");
 const filename = "./config_n.json";
 const file = require(filename);
 const yt = require("ytdl-core");
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'Suzumi > '
+});
+
+/*const objn = ("./gamble.json");
+var obj = require(objn);**/
 /*var players = [];
 var playerid = [];**/
 let queue = {};
 let tokens = {
   "prefix" : "/",
-  "passes" : 1
+  "passes" : 1,
+  "speak": 148577578146332672
 };
 
 const commands = {"play": (msg) => {
@@ -265,6 +275,133 @@ if(role.name.startsWith(`c_${colre}`)){
       msg.reply(rep[i]);
 }});
 },
+'colour' : (msg) => {
+  var args = msg.content.split(" ").slice(1);
+  let col = args[0];
+  if (col == null) {
+    msg.channel.sendMessage("\*grabs paint bucket + brush\*");
+    setTimeout(function(){
+      msg.channel.sendMessage("If you want to make me paint your name, type [/color] followed by the color you want.");
+      setTimeout(function(){
+msg.channel.sendMessage(`The list of the paint I got is the following:
+  \`\`\`xl
+  "red"
+  'darkred'
+  'salmon'
+  "orange"
+  "brown"
+  'peach'
+  "yellow"
+  "khaki"
+  "viridian"
+  "olive"
+  "green"
+  "darkgreen"
+  'lightgreen'
+  "turquoise"
+  "seagreen"
+  'aquamarine'
+  "cyan"
+  "teal"
+  'lightcyan'
+  "sky"
+  "navy"
+  'lightsky'
+  "blue"
+  "darkblue"
+  'lightblue'
+  "purple"
+  "indigo"
+  'lightpurple'
+  "pink"
+  "darkmagenta"
+  'lightpink'
+  "magenta"
+  "maroon"
+  'fuchsia'
+  "black"
+  'gray/grey'      \`\`\`    `);
+      },3000);
+    },2000);
+  } else {
+  try{
+  let colre = col.toLowerCase();
+let colvar = parseInt((file[colre]["value"]), 16);
+  msg.member.roles.filter( (role) => {
+    if(role.name.startsWith("c_")){
+      msg.member.removeRole(role);
+    }});
+    console.log(`${msg.author.username}`);
+    console.log(col);
+    console.log(colre);
+    console.log(colvar);
+      if(msg.guild.roles.exists('name', `c_${colre}`))
+      msg.guild.roles.filter( role => {
+if(role.name.startsWith(`c_${colre}`)){
+  msg.member.addRole(role).then( () => {msg.channel.sendMessage("Your name is now painted! Fancy, huh?");});
+}
+      });
+      else {
+  msg.guild.createRole({
+      name : "c_" + colre,
+      color : `${colvar}`,
+      postition : 1,
+      permissions: [
+        "CREATE_INSTANT_INVITE",
+        "ADD_REACTIONS", // add reactions to messages
+        "READ_MESSAGES",
+        "SEND_MESSAGES",
+        "CONNECT", // connect to voice
+        "SPEAK", // speak on voice
+        "CHANGE_NICKNAME"
+      ]
+    }).then(role => msg.member.addRole(role).then( () => msg.channel.sendMessage("Your name is now painted with a fresh batch of color! Fancy, huh?")));
+}
+} catch (e) {
+    if (e != "TypeError: msg.guild.roles.filter(...).then is not a function"){
+      msg.channel.sendMessage(`Someone stole my color buckets ;w; So here's the list of the paint I got right now:
+\`\`\`xl
+"red"
+'darkred'
+'salmon'
+"orange"
+"brown"
+'peach'
+"yellow"
+"khaki"
+"viridian"
+"olive"
+"green"
+"darkgreen"
+'lightgreen'
+"turquoise"
+"seagreen"
+'aquamarine'
+"cyan"
+"teal"
+'lightcyan'
+"sky"
+"navy"
+'lightsky'
+"blue"
+"darkblue"
+'lightblue'
+"purple"
+"indigo"
+'lightpurple'
+"pink"
+"darkmagenta"
+'lightpink'
+"magenta"
+"maroon"
+'fuchsia'
+"black"
+'gray/grey'\`\`\`    `);
+  }
+}
+}
+},
+
 'd' : (msg) => {
   let args = msg.content.split(" ").slice(1);
   let d = args[0];
@@ -296,11 +433,13 @@ if(role.name.startsWith(`c_${colre}`)){
 }
 },
 'eval' : (msg) => {
-  if (msg.author.id !== ('197733648403791872')) return;
-  let args = msg.content.split("|").slice(1);
-  let ev = args[0];
+  if (msg.author.id !== ('197733648403791872')) return msg.channel.sendMessage("I won't let anyone except my master to use this...\n(To prevent unauthorized shutdown, this command is locked to Yuugen only.)");
+  let args = msg.content.split(" ").slice(1);
+  var arr = [];
+  for (let i = 0; i < args.length; i++) arr.push(args[i]);
+
 try{
-    let res = eval(ev);
+    let res = eval(arr.join(" "));
     msg.channel.sendMessage(res);
 } catch (e) {
 msg.reply(e);
@@ -308,13 +447,49 @@ msg.reply(e);
 },
 
 'say' : (msg) => {
-  if (msg.author.id !== ('197733648403791872')) return;
-  msg.delete();
-  let args = msg.content.split("|").slice(1);
-  let test = args[0];
-  msg.channel.sendMessage(test);
-},
 
+  msg.delete();
+  let args = msg.content.split(" ").slice(1);
+
+  msg.channel.sendMessage(`${msg.author.username} >> ${args.join(" ")}`);
+},
+ 'math' : (msg) => {
+   let π = Math.PI;
+   let args = msg.content.split(" ").slice(1);
+   var arr = [];
+   for (let i = 0; i < args.length; i++) arr.push(args[i]);
+
+ try{
+     let res = eval(arr.join(" "));
+     msg.channel.sendMessage(res);
+ } catch (e) {
+ msg.reply(e);
+ }
+ function sin(i) {
+   return Math.sin(i);
+ }
+ function cos(i) {
+   return Math.cos(i);
+ }
+ function tan(i) {
+   return Math.tan(i);
+ }
+ function cosec(i) {
+   return 1 / Math.sin(i);
+ }
+ function sec(i) {
+   return 1 / Math.cos(i);
+ }
+ function cot(i) {
+   return 1 / Math.tan(i);
+ }
+ function sqrt(i){
+   if (i >= 0) return Math.sqrt(i);
+   if ((i < 0)&&(i != -1)) return (Math.sqrt(0-i) + "i");
+   if (i == -1) return "i";
+ }
+
+ },
 /*'blackjack' : (msg) => {
   var gameon = false;
   var cards = ['A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠',
@@ -430,6 +605,26 @@ bot.on("message", msg => {
 
 bot.on('ready', () =>{
   console.log("ready");
+  console.log("/switch 148577578146332672");
+  // /switch 148577578146332672
+  rl.setPrompt('Suzumi> ');
+  rl.prompt();
+  rl.on('line', (input) => {
+    if (input === 'rs') return;
+    if (input.startsWith("/switch")){
+      tokens.speak = input.split("/switch ").slice(1)[0];
+      console.log("Speaking at " + tokens.speak);
+  } else {
+    try {
+    bot.channels.get(`${tokens['speak']}`).sendMessage(`${input}`);
+  } catch(e) {
+    console.log(e);
+  }
+  }
+
+    rl.prompt();
+
+});
 });
 bot.login("MjY4NzgwODQ1OTg3Mzk3NjQy.C1fxMA.wRDQDEGSFBLRvALnelqnZszg2PU");
 
